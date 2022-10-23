0f7fc510b42b93a5b47baa5d4b22b2f0c0850b13
rockisch <rockischplay@gmail.com>
2022-10-23 16:43:28 -0300

    Add extra validation

--- src/cmd.c
+++ src/cmd.c
@@ -3,12 +3,16 @@
 int main(int argc, char **argv) {
     int FIXED_ARGS = 4;
     if (argc < FIXED_ARGS) {
-        printf("\nusage: main.exe <charID> <isNewChar> <logintoken> [loginmessages...]");
+        printf("usage: main.exe <charID> <isNewChar> <logintoken> [loginmessages...]");
         return 0;
     }
     uint32_t char_id = atoi(argv[1]);
     uint8_t is_new = atoi(argv[2]) > 0;
     char *logintoken = argv[3];
+    if (strlen(logintoken) != 16) {
+        fprintf(stderr, "error: logintoken must be 16 characters long");
+        return 0;
+    }
 
     char **marr = malloc(8 * sizeof(char**));
     int mlen = 0;
@@ -20,6 +24,7 @@ int main(int argc, char **argv) {
         }
     }
 
-    start(char_id, is_new, logintoken, marr, mlen);
+    uint32_t result = start(char_id, is_new, logintoken, marr, mlen);
+    printf("game closed with status %d", result);
     free(marr);
 }
